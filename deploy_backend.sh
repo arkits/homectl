3fe3c16a275e751065a4d5b43946f8bec649bab1
Archit Khode <arkits@outlook.com>
2019-10-21 22:56:33 -0700

    setup ansible for deploying backend

---
+++ deploy_backend.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# deploy_backend.sh
+
+cd deployment/ansible
+ansible-playbook deploy_backend.yml
--- scripts/run_backend.sh
+++ scripts/run_backend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # run_server.sh
 
+cd ..
 source .env/bin/activate
 pip install -r src/backend/requirements.txt
 nohup flask run > home-dash.log 2>&1 & 
