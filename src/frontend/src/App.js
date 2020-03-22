78bb213d3f9e6eb9580a584e0471cc054328b2de
Archit Khode <arkits@outlook.com>
2020-03-21 17:43:48 -0700

    initial commit of re-written frontend

---
+++ src/frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { ThemeProvider, createTheme, Arwes, Header } from "arwes";
+import PageContent from "./components/PageContent";
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={createTheme()}>
+        <Arwes animate>
+          <div style={{ padding: 20 }}>
+            <div>
+              <Header animate>
+                <h2
+                  style={{
+                    margin: 0,
+                    fontFamily: "IBM Plex Mono"
+                  }}
+                >
+                  ~/Home-Dashboard
+                </h2>
+              </Header>
+            </div>
+            <PageContent />
+          </div>
+        </Arwes>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
