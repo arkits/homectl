03e15228c4d4ed9e38544112913c7f970820ce68
Archit Khode <arkits@outlook.com>
2020-03-22 14:51:33 -0700

    log to file via log4js

--- src/backend/tools/logger.js
+++ src/backend/tools/logger.js
@@ -2,6 +2,16 @@ var log4js = require('log4js');
 
 log4js.configure({
     appenders: {
+        main: {
+            type: 'dateFile',
+            filename: '../../logs/home-dash-backend.log',
+            pattern: 'yyyy-MM-dd',
+            daysToKeep: 1,
+            layout: {
+                type: 'pattern',
+                pattern: '[%d] [%[%p%]] [%f:%l]: %m'
+            }
+        },
         out: {
             type: 'stdout',
             layout: {
@@ -12,7 +22,7 @@ log4js.configure({
     },
     categories: {
         default: {
-            appenders: ['out'],
+            appenders: ['main', 'out'],
             level: 'debug',
             enableCallStack: true
         }
