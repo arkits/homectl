ac4e70119f7a23b8b8bebb180eebd0460cd3ec7c
Archit Khode <arkits@outlook.com>
2020-03-21 20:28:45 -0700

    completed deploy_frontend script

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -9,3 +9,5 @@ yarn run build
 tar czf build.tar.gz build
 
 scp build.tar.gz pi@192.168.86.33:/home/pi/home-dash-frontend/
+
+ssh pi@192.168.86.33 "tar xf home-dash-frontend/build.tar.gz -C home-dash-frontend/"
