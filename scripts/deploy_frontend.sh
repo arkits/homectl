20b7e5a7d07de7d77cf2c9f5969456b0be0d6796
Archit Khode <arkits@outlook.com>
2020-03-21 21:51:08 -0700

    improved deploy_frontend

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+set -e
+
 cd ../src/frontend/
 
+echo ">>> building dist"
 yarn install
-
 yarn run build
 
+echo ">>> taring"
 tar czf build.tar.gz build
 
+echo ">>> scp to rpi"
 scp build.tar.gz pi@192.168.86.33:/home/pi/home-dash-frontend/
 
+echo ">>> updating the frontend"
 ssh pi@192.168.86.33 "tar xf home-dash-frontend/build.tar.gz -C home-dash-frontend/"
